b24a3faf4c77adcdef78e96e480b35c15281ed7c
Zdravko Zdravkov <zdr0@yahoo.com>
2021-03-23 16:10:21 +0100

    Demo forms

--- Workshop2/client/src/App.js
+++ Workshop2/client/src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Categories from './components/Categories';
 import PetDetails from './components/PetDetails';
+import UncontrolledDemo from './components/UncontrolledDemo';
+import ControlledDemo from './components/ControlledDemo/ControlledDemo';
+import DemoFunc from './components/DemoFunc/DemoFunc';
 
 function App() {
     return (
@@ -15,6 +18,9 @@ function App() {
                 <Route path="/" exact component={ Categories } />
                 <Route path="/categories/:category" exact component={ Categories } />
                 <Route path="/pets/details/:petId" exact component={ PetDetails }/>
+                <Route path="/uncontrolled" exact component={ UncontrolledDemo }/>
+                <Route path="/controlled" exact component={ ControlledDemo }/>
+                <Route path="/demo-func" exact component={ DemoFunc } />
             </Switch>
             <Footer />
         </div>
